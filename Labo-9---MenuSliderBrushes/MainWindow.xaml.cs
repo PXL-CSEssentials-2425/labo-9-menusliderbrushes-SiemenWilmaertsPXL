7ad83f93db907066bb8d277816d449f2c4a1d347
SiemenWilmaertsPXL <11601448@student.pxl.be>
2024-12-16 17:16:17 +0100

    afgewerkt

--- Labo-9---MenuSliderBrushes/MainWindow.xaml.cs
+++ Labo-9---MenuSliderBrushes/MainWindow.xaml.cs
@@ -16,9 +16,53 @@ namespace Labo_9___MenuSliderBrushes
     /// </summary>
     public partial class MainWindow : Window
     {
+        const string defaultValue = "2";
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void menuAfsluiten_Click(object sender, RoutedEventArgs e)
+        {
+            var result=MessageBox.Show("Wilt u afsluiten?","afsluiten",MessageBoxButton.YesNo,MessageBoxImage.Question);
+            if(MessageBox.Show("Wilt u afsluiten?","afsluiten",MessageBoxButton.YesNo,MessageBoxImage.Question)==MessageBoxResult.Yes)
+            {
+            Close();
+            }
+        }
+
+        private void menuGetal1_Click(object sender, RoutedEventArgs e)
+        {
+            numberOneTextBox.Text = defaultValue;
+        }
+
+        private void menuGetal2_Click(object sender, RoutedEventArgs e)
+        {
+            numberTwoTextBox.Text = defaultValue;
+        }
+
+
+
+        private void sliderGetal1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            numberOneTextBox.Text = Convert.ToString(sliderGetal1.Value);
+        }
+        private void sliderGetal2_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            numberTwoTextBox.Text = Convert.ToString(Math.Round(sliderGetal2.Value,2));
+        }
+
+        private void numberOneTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            double.TryParse(numberOneTextBox.Text,out double getal);
+            sliderGetal1.Value = getal;
+        }
+
+        private void numberTwoTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+            double.TryParse(numberTwoTextBox.Text, out double getal);
+            sliderGetal2.Value = getal;
         }
     }
 }
